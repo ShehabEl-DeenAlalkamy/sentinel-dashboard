851be972b1781bfdd8b5df5b66accc6b84b6a9d4
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-03-20 00:04:46 +0200

    Remove default labels as prometheus natively add them

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -4,23 +4,6 @@ import os
 import logging
 
 
-def get_instance():
-    instance = os.getenv('HOSTNAME', os.uname()[1])
-    app.logger.info(f"Set 'instance' label to '{instance}'")
-    return instance
-
-
-def get_namespace():
-    NAMESPACE_FILE = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
-    namespace = os.uname()[1]
-    if os.path.exists(NAMESPACE_FILE):
-        app.logger.info(f"Found {NAMESPACE_FILE}")
-        with open(NAMESPACE_FILE, 'r') as file:
-            namespace = file.read()
-    app.logger.info(f"Set 'namespace' label to '{namespace}'")
-    return namespace
-
-
 app = Flask(__name__)
 
 if __name__ != '__main__':
@@ -29,8 +12,7 @@ if __name__ != '__main__':
     app.logger.setLevel(gunicorn_logger.level)
 
 
-metrics = GunicornInternalPrometheusMetrics(app, defaults_prefix='frontend_service', default_labels={
-                                            'instance': get_instance(), 'namespace': get_namespace()})
+metrics = GunicornInternalPrometheusMetrics(app, defaults_prefix='frontend_service')
 
 metrics.info('app_info', 'Frontend Service',
              version='1.0.0', major='1', minor='0')
