e2dfa9e4c3f3f717bae42094339f6fa4f63dbdc7
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-03-20 20:46:56 +0200

    feat: update assets/scripts/run.sh

--- docs/assets/scripts/run.sh
+++ docs/assets/scripts/run.sh
@@ -7,7 +7,7 @@ FE_SVC_PATH="/Users/shehabeldeen/Documents/Personal/Courses/FWD/01.Cloud.Native.
 source "${FE_SVC_PATH}"/.venv/bin/activate
 
 # PROMETHEUS_MULTIPROC_DIR="${FE_SVC_PATH}"/metrics FLASK_APP="${FE_SVC_PATH}"/app.py flask run -p 8082
-cd "${FE_SVC_PATH}" && PROMETHEUS_MULTIPROC_DIR="${FE_SVC_PATH}"/metrics gunicorn --access-logfile - --error-logfile - -c config.py -w 4 -b 0.0.0.0:8082 app:app
+cd "${FE_SVC_PATH}" && PROMETHEUS_MULTIPROC_DIR="${FE_SVC_PATH}"/metrics gunicorn --access-logfile - --error-logfile - -c config.py -w 4 -t 2 -b 0.0.0.0:8082 wsgi:app
 
 deactivate
 
