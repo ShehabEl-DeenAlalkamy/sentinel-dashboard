9244c21b0a17a0d56eb9be3fbcac8492e53a5505
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-03-24 21:30:18 +0200

    feat: auto instrument frontend using OTEL

--- reference-app/frontend/app/__init__.py
+++ reference-app/frontend/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 import logging
 
 _logger = logging.getLogger('frontend_service')
 metrics = GunicornInternalPrometheusMetrics.for_app_factory(
     defaults_prefix='frontend_service', excluded_paths=['/metrics', '/health'])
+
+flask_instrumentor = FlaskInstrumentor()
+tracer = trace.get_tracer(__name__)
 
 
 def create_app(env=None):
@@ -21,6 +26,8 @@ def create_app(env=None):
 
     app.register_blueprint(index_bp)
     app.register_blueprint(healthchecks_bp)
+
+    flask_instrumentor.instrument_app(app, excluded_urls="health/*,metrics")
 
     with app.app_context():
         from app.metrics import _init as init_metrics
