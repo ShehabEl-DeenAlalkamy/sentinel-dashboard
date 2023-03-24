779b872bc49ff870ebf38a7a076cc7be84b05315
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-03-24 11:00:42 +0200

    temp: sleep 3 secs at /api for traces testing

--- reference-app/backend/app/routes/apis.py
+++ reference-app/backend/app/routes/apis.py
@@ -1,9 +1,14 @@
+from app import tracer
+
 from flask import Blueprint, jsonify
+import time
 
 apis_bp = Blueprint('apis_bp', __name__)
 
 
 @apis_bp.route("/api", methods=["GET"])
 def my_api():
+    with tracer.start_as_current_span("Sleep 3 seconds"):
+        time.sleep(3)
     answer = "something"
     return jsonify(repsonse=answer)
