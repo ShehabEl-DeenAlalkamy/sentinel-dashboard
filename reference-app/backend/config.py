647102c885000e44b9f4c6949b13be1750595c46
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-03-24 11:00:10 +0200

    feat: implement fork hooks for gunicorn BatchSpanProcessor() to be fork-safe

--- reference-app/backend/config.py
+++ reference-app/backend/config.py
@@ -1,6 +1,29 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 
 def child_exit(server, worker):
     GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(
         worker.pid)
+
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+    resource = Resource.create(
+        attributes={
+            "service.name": "backend-service",
+            "worker": worker.pid,
+        }
+    )
+
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+
+    span_processor = BatchSpanProcessor(
+        OTLPSpanExporter(endpoint="http://localhost:4317")
+    )
+    trace.get_tracer_provider().add_span_processor(span_processor)
